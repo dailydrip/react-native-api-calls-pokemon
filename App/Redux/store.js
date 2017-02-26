f0f43b742baf881151f23eda6c6e591a5d1d2c19
Franzé Jr <franzejr@gmail.com>
2017-02-26 16:03:58 -0300

    Change to Redux Loop -- :metal:

--- App/Redux/store.js
+++ App/Redux/store.js
@@ -1,28 +1,17 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-
+import { createStore, compose } from 'redux'
 import reducer from './reducers'
-import Sagas from './../Sagas'
 import Immutable from 'immutable'
 import Pokemon from '../../models/Pokemon'
 
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware()
+import * as Loop from 'redux-loop'
 
 const initialState = Immutable.fromJS({ pokemon: new Pokemon(), loading: false, error: '' })
 
 export default makeStore = () => {
-  var store = createStore(
-    reducer,
-    initialState,
-    compose(applyMiddleware(sagaMiddleware))
-  )
+
+  const store = createStore(reducer, initialState, Loop.install());
 
   console.log('creating store', store)
 
-  // then run the saga
-  sagaMiddleware.run(Sagas)
   return store
 }
-
-
